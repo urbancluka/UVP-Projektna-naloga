3fdf75e1dc4f0cf0cf8a6161d93462c9662b0650
Luka Urbanč <urbancluka3@gmail.com>
2024-08-03 17:55:57 +0200

    fix bug with acceleration and consumption

---
+++ save_csv.py
@@ -0,0 +1,4 @@
+#This file contains the functions related to saving data stored in python objects into organised .csv files.
+import csv
+#              id  price   transmission                     body type           sets drive    fuel type  cylinders power   torque topspeed  acceleration    consumption
+list_of_data = [2, 25500, '5 speed manual transmission', '3-doors, convertible', 4, 'front', 'gasoline', 4, 1368, 'power', 206,      205,   7.9,            65]
