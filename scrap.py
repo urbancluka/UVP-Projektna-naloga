f62f224550bdfb4bc847ff6addbc30bd8e437323
Luka Urbanč <urbancluka3@gmail.com>
2024-08-03 15:44:34 +0200

    changed data structure on extract_name_and_id

--- scrap.py
+++ scrap.py
@@ -10,16 +10,15 @@ def get_urls_from_main(file):
         return urls
     
 def extract_name_and_id(list_of_urls):
-    """Function takes a list of urls and returns a dict of the form {id: [name, url]}. Used then to save car pages"""
-    dict_id = {}
+    """Function takes a list of urls and returns a list of the form [[id, name, url]]. Used then to save car pages"""
+    list_id = []
     for url in list_of_urls:
         re_id = r'-specs/(\d*)'
         id = int(re.search(re_id, url).group()[7:])
         re_name = r'https://www.cars-data.com/en/(.*?)-specs'
         name = re.search(re_name, url).group()[29:-6]
-        dict_id[id] = [name, url]
-    print(dict_id)
-    return dict_id
+        list_id.append([id, name, url])
+    return list_id
 
 def get_content_from_car_page(file):
     """File extracts data for given car. Returns a dict. Data: price, transmission, body type, number of seats, drive wheel, fuel type, engine capacity, power(kW/hp), max torque, top speed, acceleration, consumption"""
