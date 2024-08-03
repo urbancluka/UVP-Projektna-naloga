4ae781961ea866c63809c4943eb0eda8c103d19c
Luka Urbanč <urbancluka3@gmail.com>
2024-08-03 14:31:04 +0200

    add download_cars()

--- download_file.py
+++ download_file.py
@@ -1,4 +1,4 @@
-#This file contains the function to download the .html file from the internet and saves it to local memory.
+#This file contains the function to download the .html file from the internet and saves it to local memory. 
 import requests
 
 
@@ -15,7 +15,7 @@ max_page_number = 5
 
 #https://www.cars-data.com/en/all-cars/page1.html -- https://www.cars-data.com/en/all-cars/page97.html
 
-def download(i, file_name):
+def download_main(i, file_name):
     url = f"https://www.cars-data.com/en/all-cars/page{i}.html"
     """
     Function takes the page number of desired site and the names of the file, to be saved as. Function returns None.
@@ -31,5 +31,20 @@ def download(i, file_name):
         print(f"Error! erroe code: {response.status_code}")
     return
 
-for index in range(starting_page_number, max_page_number + 1):
-    download(index, f"page{index}")
+#for index in range(starting_page_number, max_page_number + 1):
+#   download_main(index, f"page{index}")
+
+#To be merged together:
+def download_cars(url, file_name):
+    """Almost identical to download_main, but saves to different folder. """
+    response = requests.get(url)
+    print(f"URL of downloaded website: {response.url}")
+    if response.ok:
+        print("Response OK. Downloading site...")
+        with open(f"cars/{file_name}", "wb") as file:
+            file.write(response.content)
+            print("Website saved to memory")
+    else:
+        print(f"Error! erroe code: {response.status_code}")
+    return
+
