ce7c33bada0a2de5ccb11330c325c2987c660746
Luka Urbanč <urbancluka3@gmail.com>
2024-08-03 22:13:59 +0200

    made file_name an optional para in download_main

--- download_file.py
+++ download_file.py
@@ -18,7 +18,7 @@ max_page_number = 5
 
 #https://www.cars-data.com/en/all-cars/page1.html -- https://www.cars-data.com/en/all-cars/page97.html
 
-def download_main(i, file_name):
+def download_main(i, file_name=f"page{i}.html"):
     url = f"https://www.cars-data.com/en/all-cars/page{i}.html"
     """
     Function takes the page number of desired site and the names of the file, to be saved as. Function returns None.
