a3dc4ddbf9364517d468eab42a8df74cef3b8675
Luka Urbanč <urbancluka3@gmail.com>
2024-08-03 13:30:18 +0200

    update download function

--- download_file.py
+++ download_file.py
@@ -2,25 +2,34 @@
 import requests
 
 
-#Set website url
-url = ""
+#Set website url:
+#url = f"https://www.cars-data.com/en/all-cars/page{page_number}.html"
+
 #Set file_name:
-file_name = ""
-#Number of pages:
-pages = 0
+#file_name = f"page{page_number}"
+
+#Starting page number:
+starting_page_number = 1
+#Max page number:
+max_page_number = 5
+
 #https://www.cars-data.com/en/all-cars/page1.html -- https://www.cars-data.com/en/all-cars/page97.html
 
-def download(url, file_name):
+def download(i, file_name):
+    url = f"https://www.cars-data.com/en/all-cars/page{i}.html"
     """
-    Function takes the url of desired site and the names of the file, to be saved as. Function returns None.
+    Function takes the page number of desired site and the names of the file, to be saved as. Function returns None.
     """
     response = requests.get(url)
     print(f"URL of downloaded website: {response.url}")
     if response.ok:
+        print("Response OK. Downloading site...")
         with open(f"websites/{file_name}", "wb") as file:
             file.write(response.content)
+            print("Website saved to memory")
     else:
         print(f"Error! erroe code: {response.status_code}")
-        return
+    return
 
-download(url, file_name)
+for index in range(starting_page_number, max_page_number + 1):
+    download(index, f"page{index}")
