bd3aec3d6b32031fd8258f879e4449051062a12d
Luka Urbanč <urbancluka3@gmail.com>
2024-08-11 18:59:14 +0200

    style file

--- save_csv.py
+++ save_csv.py
@@ -1,5 +1,6 @@
 #This file contains the functions related to saving data stored in python objects into organised .csv files.
 import csv
+import pandas as pd 
 #              id  price   transmission                     body type           seats drive    fuel type  cylinders power   torque topspeed  acceleration    consumption
 #list_of_data = [2, 25500, '5 speed manual transmission', '3-doors, convertible', 4, 'front', 'gasoline', 4, 1368, 'power', 206,      205,   7.9,            6.5]
 
@@ -25,11 +26,10 @@ def create_main_csv(csv_name, list_of_fields=["Id",
 
 def save_list_to_csv(list_of_data, file_name="data.csv"):
     """Function writes list of data into an existing .csv file."""
-    with open(f"{file_name}", "a") as f:
-        #add a check if file is empty: if so, stop the function.
-        #add check to see if lenth of list_of_data is the same as csv head
+    with open(f"{file_name}", "a+") as f:
         write = csv.writer(f)
         write.writerow(list_of_data)
 
     
 
+save_list_to_csv([1, 3, 4, 5], "test.csv")
