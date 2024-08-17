2f150bcc679c7d1f628c9ceee0a53d9a23d54e97
Luka Urbanč <urbancluka3@gmail.com>
2024-08-17 13:19:21 +0200

    improve error catching

--- scrap.py
+++ scrap.py
@@ -48,105 +48,116 @@ def get_content_from_car_page(id):
         content = f.read()
 
         re_price = r'Price:</td><td class="col-6 grey">&euro; (.*?)</td></tr>'
-        price = re.findall(re_price, content)[0]
-        price = re.sub("[.]", "", price)
         try:
+            price = re.findall(re_price, content)[0]
+            price = re.sub("[.]", "", price)
             price = int(price)
-        except TypeError:
+        except (ValueError, IndexError) as e:
             price = "NaN"
         list_of_data.append(price)
         
 
         re_transmission = r'Transmission:</td><td class="col-6 grey">(.*?)</td>'
-        transmission = re.findall(re_transmission, content)[0]
+        try:
+            transmission = re.findall(re_transmission, content)[0]
+        except (ValueError, IndexError) as e:
+            transmission = "NaN"
         list_of_data.append(transmission)
 
         re_number_of_gears = r'Transmission:</td><td class="col-6 grey">(.*?)</td>'
-        number_of_gears = re.findall(re_number_of_gears, content)[0]
-        number_of_gears = number_of_gears[0]
         try:
+            number_of_gears = re.findall(re_number_of_gears, content)[0]
+            number_of_gears = number_of_gears[0]
             number_of_gears = int(number_of_gears)
-        except TypeError:
+        except (ValueError, IndexError) as e:
             number_of_cylinders = "NaN"
         list_of_data.append(number_of_gears)
 
         re_body_type = r'Body Type:</td><td class="col-6">(.*?)</td>'
-        body_type = re.findall(re_body_type, content)[0]
+        try:
+            body_type = re.findall(re_body_type, content)[0]
+        except (ValueError, IndexError) as e:
+            body_type = "NaN"
         list_of_data.append(body_type)
 
         re_number_of_seats = r'Number Of Seats:</td><td class="col-6">(.*?)</td>'
-        number_of_seats = re.findall(re_number_of_seats, content)[0]
         try:
+            number_of_seats = re.findall(re_number_of_seats, content)[0]
             number_of_seats = int(number_of_seats)
-        except TypeError:
+        except (ValueError, IndexError) as e:
             number_of_seats = "NaN"
         list_of_data.append(number_of_seats)
 
         re_drive_wheel = r'Drive Wheel :</td><td class="col-6 grey">(.*?)</td>'
-        drive_wheel = re.findall(re_drive_wheel, content)[0]
+        try:
+            drive_wheel = re.findall(re_drive_wheel, content)[0]
+        except (ValueError, IndexError) as e:
+            drive_wheel = "NaN"
         list_of_data.append(drive_wheel)
 
         re_fuel_type = r'Fuel Type:</td><td class="col-6 grey">(.*?)</td>'
-        fuel_type = re.findall(re_fuel_type, content)[0]
+        try:
+            fuel_type = re.findall(re_fuel_type, content)[0]
+        except (ValueError, IndexError) as e:
+            fuel_type = "NaN"
         list_of_data.append(fuel_type)
 
         re_number_of_cylinders = r'Cylinders:</td><td class="col-6 grey">(.*?)</td>'
-        number_of_cylinders = re.findall(re_number_of_cylinders, content)[0]
-        number_of_cylinders = re.sub("[^0-9]", "", number_of_cylinders)
         try:
+            number_of_cylinders = re.findall(re_number_of_cylinders, content)[0]
+            number_of_cylinders = re.sub("[^0-9]", "", number_of_cylinders)
             number_of_cylinders = int(number_of_cylinders)
-        except TypeError:
+        except (ValueError, IndexError) as e:
             number_of_cylinders = "NaN"
         list_of_data.append(number_of_cylinders)
 
         re_engine_capacity = r'Engine Capacity:</td><td class="col-6 grey">(.*?) cc</td>'
-        engine_capacity = re.findall(re_engine_capacity, content)[0]
         try:
+            engine_capacity = re.findall(re_engine_capacity, content)[0]
             engine_capacity = int(engine_capacity)
-        except TypeError:
+        except (ValueError, IndexError) as e:
             engine_capacity = "NaN"
         list_of_data.append(engine_capacity)
 
-        #Power doesnt work
         re_power = r'Total Max. Power \(hp\):</td><td class="col-6">(.*?)</td></tr><tr class="'
-        power = re.findall(re_power, content)[0]
         try:
+            power = re.findall(re_power, content)[0]
             power = int(power)
-        except TypeError:
+        except (ValueError, IndexError) as e:
             power = "NaN"
         list_of_data.append(power)
 
         re_torque = r'Max Torque:</td><td class="col-6 grey">(.*?) nm</td>'
-        torque = re.findall(re_torque, content)[0]
         try:
+            torque = re.findall(re_torque, content)[0]
             torque = int(torque)
-        except TypeError:
+        except (ValueError, IndexError) as e:
             torque = "NaN"
         list_of_data.append(torque)
 
         re_top_speed = r'Top Speed:</td><td class="col-6 grey">(.*?) km/h</td>'
-        top_speed = re.findall(re_top_speed, content)[0]
         try:
+            top_speed = re.findall(re_top_speed, content)[0]
             top_speed = float(top_speed)
-        except TypeError:
+        except (ValueError, IndexError) as e:
             top_speed = "NaN"
         list_of_data.append(top_speed)
 
         re_acceleration = r'Acceleration 0-100 Km / H:</td><td class="col-6">(.*?) s</td>'
-        acceleration = re.findall(re_acceleration, content)[0]
-        acceleration = re.sub(",", r".", acceleration)
         try:
+            acceleration = re.findall(re_acceleration, content)[0]
+            acceleration = re.sub(",", r".", acceleration)
             acceleration = float(acceleration)
-        except TypeError:
+        except (ValueError, IndexError) as e:
             acceleration = "NaN"
         list_of_data.append(acceleration)
 
         re_combined_consumption = r'Combined Consumption:</td><td class="col-6 grey">(.*?) l/100km</td>'
-        combined_consumption = re.findall(re_combined_consumption, content)[0]
-        combined_consumption = re.sub(",", r".", combined_consumption)
         try:
+            combined_consumption = re.findall(re_combined_consumption, content)[0]
+            combined_consumption = re.sub(",", r".", combined_consumption)
             combined_consumption = float(combined_consumption)
-        except TypeError:
+        except (ValueError, IndexError) as e:
             combined_consumption = "NaN"
         list_of_data.append(combined_consumption)
 
