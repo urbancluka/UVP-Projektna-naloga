b5c55ac555b69b6385ac754952e1f8f929f4ecd2
Luka Urbanč <urbancluka3@gmail.com>
2024-08-17 13:19:34 +0200

    remove bug in loop

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main():
     print("Pozdravljeni v programu za pridobivanje podatkov s spletne strani cars-data.com. \n")
     print("Program vas bo prosil za vnos stevilk. To storite tako, da vnesete stevilko, nato pa pritisnite 'Enter' na vasi tipkovnici.")
     while not valid_start_stop_conditions:
-        starting_page = input("Prosimo, vnesite zacetno stran (stevilko), na kateri bi zaceli s zajemom podatkov. Stevilo mora biti manj od 97. Preglagamo, da zacnete z 1:\n")
+        starting_page = input("Prosimo, vnesite zacetno stran (stevilko), na kateri bi zaceli s zajemom podatkov. Stevilo mora biti manj od 97. Predlagamo, da zacnete z 1:\n")
         if starting_page.isnumeric():
             if int(starting_page) in range(1, 98):
                 starting_page = int(starting_page)
@@ -45,7 +45,7 @@ def main():
                 else:
                     print("Prosim vnesite stevilo.")
             else:
-                print("Stevilka ni veljavna. Ustrezati mora 1 <= stevilka < 97")
+                print("Stevilka ni veljavna. Ustrezati mora 1 <= stevilka <= 97")
         else:
             print("To ni veljaven znak")
 
@@ -57,12 +57,13 @@ def main():
 
     ############################## RETRIEVE DATA FOR EVERY FILE IN WEBSITES/ ##############################
     list_of_files = os.listdir("websites/")
+
+    scsv.create_main_csv("auxillary.csv", list_of_fields=["Id", "Name", "Url"])
+    scsv.create_main_csv("data.csv")
     for file in list_of_files:
         urls = sc.get_urls_from_main(file)
         id_name_url = sc.extract_name_and_id(urls)
-        scsv.create_main_csv("auxillary.csv", list_of_fields=["Id", "Name", "Url"])
         dnf.download_cars(id_name_url)
-        scsv.create_main_csv("data.csv")
         for temp_list in id_name_url:
             scsv.save_list_to_csv(temp_list, file_name="auxillary.csv")
             data = sc.get_content_from_car_page(temp_list[0])
