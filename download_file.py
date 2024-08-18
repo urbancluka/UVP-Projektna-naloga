29f26ba17276212293e16bfa4ddb4b5db137bb51
Luka Urbanč <urbancluka3@gmail.com>
2024-08-18 16:41:44 +0200

    fix comment styling

--- download_file.py
+++ download_file.py
@@ -1,22 +1,10 @@
-#This file contains the functions to download the .html files from the internet and saves it to local memory. 
-#download_main() for main page downloads,saved to "websites"
-#download_cars() saves the spefific cars website to the folder "cars"
+"""
+This file contains the functions to download the .html files from the internet and saves it to local memory. 
+download_main() for main page downloads, saved to "websites" folder
+download_cars() saves the specific cars website to the folder "cars" folder
+"""
 
 import requests
-
-
-#Set website url:
-#url = f"https://www.cars-data.com/en/all-cars/page{page_number}.html"
-
-#Set file_name:
-#file_name = f"page{page_number}"
-
-#Starting page number:
-starting_page_number = 1
-#Max page number:
-max_page_number = 5
-
-#https://www.cars-data.com/en/all-cars/page1.html -- https://www.cars-data.com/en/all-cars/page97.html
 
 def download_main(i):
     url = f"https://www.cars-data.com/en/all-cars/page{i}.html"
@@ -24,32 +12,26 @@ def download_main(i):
     Function takes the page number of desired site and the names of the file, to be saved as. Function returns None.
     """
     response = requests.get(url)
-    print(f"URL of downloaded website: {response.url}")
-    if response.ok:
+    print(f"URL of downloaded website: {response.url}") # Get URL of website
+    if response.ok: #Test response code
         print("Response OK. Downloading site...")
         with open(f"websites/page{i}.html", "wb") as file:
-            file.write(response.content)
+            file.write(response.content) # Save file to memory
             print("Website saved to memory")
     else:
         print(f"Error! error code: {response.status_code}")
         return
+    
 
-#This loop could be built into the download_main functon
-#for index in range(starting_page_number, max_page_number + 1):
-#   download_main(index, f"page{index}")
-
-
-#https://www.cars-data.com/en/abarth-500-1-4-16v-t-jet-specs/1
 def download_cars(list_id):
     """Function takes a list of type [[id, name, url]] and saves the site under the id"""
     for item in list_id:
-        response = requests.get(item[2])
-        #url = https://www.cars-data.com/en/abarth-500-1-4-16v-t-jet-specs/123455
+        response = requests.get(item[2]) # Get URL of website
         print(f"URL of downloaded website: {response.url}")
-        if response.ok:
+        if response.ok: # Check response
             print("Response OK. Downloading site...")
             with open(f"cars/{item[0]}.html", "wb") as file:
-                file.write(response.content)
+                file.write(response.content)  # Save to memory
                 print("Website saved to memory")
         else:
             print(f"Error! error code: {response.status_code} at url {item[2]}")
@@ -57,8 +39,8 @@ def download_cars(list_id):
     return
 
 
-#TEST CASE
-#download_cars([[2, 'abarth-500c-1-4-16v-t-jet', 'https://www.cars-data.com/en/abarth-500c-1-4-16v-t-jet-specs/2']])
+# TEST CASE
+# download_cars([[2, 'abarth-500c-1-4-16v-t-jet', 'https://www.cars-data.com/en/abarth-500c-1-4-16v-t-jet-specs/2']])
 
 
 
